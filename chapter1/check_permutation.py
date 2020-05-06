b5ddbdb69d7fba9fa79abac9367efe12d4636800
Matt Colen <mattcolen@gmail.com>
2020-05-06 16:35:34 -0700

    Solve problem 1.2.

---
+++ chapter1/check_permutation.py
@@ -0,0 +1,12 @@
+"""Solution to 1.2 Check Permutation.
+
+Given two strings, write a method to decide if one is a permutation of
+the other.
+"""
+
+from collections import Counter
+
+
+def are_permutations(s1: str, s2: str) -> bool:
+    """Returns True if s1 is a permutation of s2."""
+    return Counter(s1) == Counter(s2)
---
+++ chapter1/test_check_permutation.py
@@ -0,0 +1,21 @@
+"""Tests for solution to 1.2 Check Permutation."""
+
+import unittest
+
+from check_permutation import are_permutations
+
+
+class TestCheckPermutation(unittest.TestCase):
+    """Simple tests of string permutability."""
+    def test_apple_papel_are_permutations(self):
+        self.assertTrue(are_permutations('apple', 'papel'))
+
+    def test_carrot_tarroc_are_permutations(self):
+        self.assertTrue(are_permutations('carrot', 'tarroc'))
+
+    def test_hello_llloh_are_not_permutations(self):
+        self.assertFalse(are_permutations('hello', 'llloh'))
+
+
+if __name__ == '__main__':
+    unittest.main()
