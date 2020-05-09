1e4980bf243c86fb1ba061174b11324b6e68485b
Matt Colen <mattcolen@gmail.com>
2020-05-09 00:44:03 -0700

    Solve with O(1) space.

--- chapter1/zero_matrix.py
+++ chapter1/zero_matrix.py
@@ -10,11 +10,24 @@ from typing import Sequence
 def zero_matrix(matrix: Sequence[Sequence[int]]) -> None:
     """Sets entire row and column to 0 for all 0 elements in matrix."""
     m, n = len(matrix), len(matrix[0])
-    rows = [i for i in range(m) if any(matrix[i][j] == 0 for j in range(n))]
-    cols = [j for j in range(n) if any(matrix[i][j] == 0 for i in range(m))]
-    for i in rows:
+    first_row_zero = 0 in matrix[0]
+    first_col_zero = any(row[0] == 0 for row in matrix)
+    # Identify rows with a 0.
+    for row in matrix:
+        if 0 in row:
+            row[0] = 0
+    # Identify cols with a 0.
+    for j in range(n):
+        if any(row[j] == 0 for row in matrix):
+            matrix[0][j] = 0
+    # Set necessary elements to 0.
+    for i in range(1, m):
+        for j in range(1, n):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+    if first_row_zero:
         for j in range(n):
-            matrix[i][j] = 0
-    for j in cols:
+            matrix[0][j] = 0
+    if first_col_zero:
         for i in range(m):
-            matrix[i][j] = 0
+            matrix[i][0] = 0
