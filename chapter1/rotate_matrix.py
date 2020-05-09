a2c239a6090e78dcd0ad717e4265cf939be7b8df
Matt Colen <mattcolen@gmail.com>
2020-05-08 23:55:51 -0700

    Solve 1.7 Rotate Matrix

---
+++ chapter1/rotate_matrix.py
@@ -0,0 +1,20 @@
+"""Solution to 1.7 Rotate Matrix.
+
+Given an image represented by an N x N matrix, where each pixel in the
+image is represented by an integer, write a method to rotate the image
+by 90 degrees. Can you do this in place?
+"""
+
+from typing import Sequence
+
+
+def rotate_matrix(matrix: Sequence[Sequence[int]]) -> None:
+    """Rotates matrix by 90 degrees in place."""
+    n = len(matrix)
+    for i in range((n + 1) // 2):
+        for j in range(n // 2):
+            x, y, tmp = i, j, matrix[i][j]
+            for _ in range(3):
+                matrix[x][y] = matrix[n - y - 1][x]
+                x, y = n - y - 1, x
+            matrix[x][y] = tmp
---
+++ chapter1/test_rotate_matrix.py
@@ -0,0 +1,31 @@
+"""Tests for solution to 1.7 Rotate Matrix."""
+
+import unittest
+
+from chapter1.rotate_matrix import rotate_matrix
+
+
+class TestRotateMatrix(unittest.TestCase):
+    """Simple tests of matrix rotation."""
+
+    def test_five_by_five_matrix(self):
+        matrix = [
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20],
+            [21, 22, 23, 24, 25],
+        ]
+        rotated = [
+            [21, 16, 11, 6, 1],
+            [22, 17, 12, 7, 2],
+            [23, 18, 13, 8, 3],
+            [24, 19, 14, 9, 4],
+            [25, 20, 15, 10, 5],
+        ]
+        rotate_matrix(matrix)
+        self.assertEqual(rotated, matrix)
+
+
+if __name__ == '__main__':
+    unittest.main()
