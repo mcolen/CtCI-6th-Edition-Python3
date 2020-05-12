cd9726a61e82751a15da2782492cb3421d567eb2
mcolen <mattcolen@gmail.com>
2020-05-12 10:52:32 -0700

    Clarify function calls with keyword arguments

--- chapter2/tests/test_return_kth_to_last.py
+++ chapter2/tests/test_return_kth_to_last.py
@@ -12,35 +12,35 @@ class TestReturnKthToLast(unittest.TestCase):
         head.next = Node(1)
         head.next.next = Node(2)
         head.next.next.next = Node(3)
-        self.assertIsNone(kth_to_last(head, 0))
+        self.assertIsNone(kth_to_last(head, k=0))
 
     def test_last(self):
         head = Node(0)
         head.next = Node(1)
         head.next.next = Node(2)
         head.next.next.next = Node(3)
-        self.assertEqual(head.next.next.next, kth_to_last(head, 1))
+        self.assertEqual(head.next.next.next, kth_to_last(head, k=1))
 
     def test_2nd_to_last(self):
         head = Node(0)
         head.next = Node(1)
         head.next.next = Node(2)
         head.next.next.next = Node(3)
-        self.assertEqual(head.next.next, kth_to_last(head, 2))
+        self.assertEqual(head.next.next, kth_to_last(head, k=2))
 
     def test_3rd_to_last(self):
         head = Node(0)
         head.next = Node(1)
         head.next.next = Node(2)
         head.next.next.next = Node(3)
-        self.assertEqual(head.next, kth_to_last(head, 3))
+        self.assertEqual(head.next, kth_to_last(head, k=3))
 
     def test_4th_to_last(self):
         head = Node(0)
         head.next = Node(1)
         head.next.next = Node(2)
         head.next.next.next = Node(3)
-        self.assertEqual(head, kth_to_last(head, 4))
+        self.assertEqual(head, kth_to_last(head, k=4))
 
 
 if __name__ == '__main__':
