61080011d121eeb4be45c5ed61ca14a5ce960120
Matt Colen <mattcolen@gmail.com>
2020-05-11 19:34:10 -0700

    Solve 2.3 Delete Middle Node

---
+++ chapter2/delete_middle_node.py
@@ -0,0 +1,17 @@
+"""Solution to 2.3 Delete Middle Node.
+
+Implement an algorithm to delete a node in the middle (i.e. any node
+but the first and last node, not necessarily the exact middle) of a
+singly linked list, given only access to that node.
+
+EXAMPLE
+Input: The node `c` from the linked list a->b->c->d->e->f
+Result: Nothing is returned, but the new list looks like a->b->d->e->f
+"""
+
+from chapter2.node import Node
+
+
+def delete_middle_node(node: Node) -> None:
+    """Deletes given middle node from its linked list."""
+    node.data, node.next = node.next.data, node.next.next
---
+++ chapter2/tests/test_delete_middle_node.py
@@ -0,0 +1,29 @@
+"""Tests for 2.3 Delete Middle Node."""
+
+import unittest
+
+from chapter2.delete_middle_node import delete_middle_node
+from chapter2.node import Node
+
+
+class TestDeleteMiddleNode(unittest.TestCase):
+    def test_delete_c_from_abcdef(self):
+        head = Node('a')
+        head.next = Node('b')
+        head.next.next = Node('c')
+        head.next.next.next = Node('d')
+        head.next.next.next.next = Node('e')
+        head.next.next.next.next.next = Node('f')
+
+        head_deleted_c = Node('a')
+        head_deleted_c.next = Node('b')
+        head_deleted_c.next.next = Node('d')
+        head_deleted_c.next.next.next = Node('e')
+        head_deleted_c.next.next.next.next = Node('f')
+
+        delete_middle_node(head.next.next)
+        self.assertEqual(head_deleted_c, head)
+
+
+if __name__ == '__main__':
+    unittest.main()
