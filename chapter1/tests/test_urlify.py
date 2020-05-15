82c12ab1f75975d6463602cfb2e6abae70b51bc4
Matt Colen <mattcolen@gmail.com>
2020-05-14 22:47:04 -0700

    Make test more explicit

--- chapter1/tests/test_urlify.py
+++ chapter1/tests/test_urlify.py
@@ -9,8 +9,8 @@ class TestURLify(unittest.TestCase):
 
     def test_mr_john_smith(self):
         s = list('mr john smith    ')
-        urlify(s, len(''.join(s).strip()))
-        self.assertEqual(list('mr%20john%20smith'), s)
+        urlify(s, length=13)
+        self.assertEqual('mr%20john%20smith', ''.join(s))
 
 
 if __name__ == '__main__':
