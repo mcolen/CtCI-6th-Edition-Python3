9169e9ecb6403bd391bccfea5ad3e5e225da3371
Matt Colen <mattcolen@gmail.com>
2020-05-20 00:12:25 -0700

    Use 7-node minimal BST in tests instead of 10
    7-node minimal BST is fully specified. A different implementation of
    minimal_bst() could not produce a different tree.

--- chapter4/tests/test_first_common_ancestor.py
+++ chapter4/tests/test_first_common_ancestor.py
@@ -9,19 +9,19 @@ from chapter4.minimal_tree import minimal_bst
 class TestFirstCommonAncestor(unittest.TestCase):
 
     def test_no_common_ancestor(self) -> None:
-        bst1 = minimal_bst(range(10))
-        bst2 = minimal_bst(range(10))
+        bst1 = minimal_bst(range(7))
+        bst2 = minimal_bst(range(7))
         assert bst1.left and bst2.right
         self.assertIsNone(first_common_ancestor(bst1.left, bst2.right))
 
     def test_same_depth(self) -> None:
-        bst = minimal_bst(range(10))
+        bst = minimal_bst(range(7))
         assert bst.left and bst.left.left and bst.left.right
         self.assertIs(bst.left,
                       first_common_ancestor(bst.left.left, bst.left.right))
 
     def test_different_depth(self) -> None:
-        bst = minimal_bst(range(10))
+        bst = minimal_bst(range(7))
         assert bst.left and bst.right and bst.right.right
         self.assertIs(bst, first_common_ancestor(bst.left, bst.right.right))
 
--- chapter4/tests/test_successor.py
+++ chapter4/tests/test_successor.py
@@ -26,73 +26,53 @@ def _find_or_die(root: TreeNode, value: int) -> TreeNode:
 class TestSuccessor(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.bst_10 = minimal_bst(range(10))
+        # minimal_bst(range(7)) is fully specified and unique.
+        self.bst_7 = minimal_bst(range(7))
 
     def test_0(self) -> None:
-        node = _find_or_die(self.bst_10, 0)
+        node = _find_or_die(self.bst_7, 0)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(1, res.value)  # type: ignore[union-attr]
 
     def test_1(self) -> None:
-        node = _find_or_die(self.bst_10, 1)
+        node = _find_or_die(self.bst_7, 1)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(2, res.value)  # type: ignore[union-attr]
 
     def test_2(self) -> None:
-        node = _find_or_die(self.bst_10, 2)
+        node = _find_or_die(self.bst_7, 2)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(3, res.value)  # type: ignore[union-attr]
 
     def test_3(self) -> None:
-        node = _find_or_die(self.bst_10, 3)
+        node = _find_or_die(self.bst_7, 3)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(4, res.value)  # type: ignore[union-attr]
 
     def test_4(self) -> None:
-        node = _find_or_die(self.bst_10, 4)
+        node = _find_or_die(self.bst_7, 4)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(5, res.value)  # type: ignore[union-attr]
 
     def test_5(self) -> None:
-        node = _find_or_die(self.bst_10, 5)
+        node = _find_or_die(self.bst_7, 5)
         res = successor(node)
         self.assertIsNotNone(res)
         # https://github.com/python/mypy/issues/4063
         self.assertEqual(6, res.value)  # type: ignore[union-attr]
 
     def test_6(self) -> None:
-        node = _find_or_die(self.bst_10, 6)
-        res = successor(node)
-        self.assertIsNotNone(res)
-        # https://github.com/python/mypy/issues/4063
-        self.assertEqual(7, res.value)  # type: ignore[union-attr]
-
-    def test_7(self) -> None:
-        node = _find_or_die(self.bst_10, 7)
-        res = successor(node)
-        self.assertIsNotNone(res)
-        # https://github.com/python/mypy/issues/4063
-        self.assertEqual(8, res.value)  # type: ignore[union-attr]
-
-    def test_8(self) -> None:
-        node = _find_or_die(self.bst_10, 8)
-        res = successor(node)
-        self.assertIsNotNone(res)
-        # https://github.com/python/mypy/issues/4063
-        self.assertEqual(9, res.value)  # type: ignore[union-attr]
-
-    def test_9(self) -> None:
-        node = _find_or_die(self.bst_10, 9)
+        node = _find_or_die(self.bst_7, 6)
         res = successor(node)
         self.assertIsNone(res)
 
