9165147542322c58f747a1ff5d288cc2b90c0c03
Matt Colen <mattcolen@gmail.com>
2020-05-26 21:36:28 -0700

    Delete extraneous line in module docstring

--- chapter1/palindrome_permutation.py
+++ chapter1/palindrome_permutation.py
@@ -5,7 +5,6 @@ palindrome. A palindrome is a word or phrase that is the same forwards
 and backwards. A permutation is a rearrangement of letters. The
 palindrome does not need to be limited to just dictionary words. You
 can ignore casing and non-letter characters.
-the other.
 
 EXAMPLE
 Input:  Tact Coa
