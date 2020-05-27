31df9c70f9df726b38225d793ef1c29f7698ef7e
Matt Colen <mattcolen@gmail.com>
2020-05-26 21:09:28 -0700

    Make assert more readable

--- chapter1/tests/test_urlify.py
+++ chapter1/tests/test_urlify.py
@@ -10,7 +10,7 @@ class TestURLify(unittest.TestCase):
     def test_mr_john_smith(self) -> None:
         s = list('mr john smith    ')
         urlify.urlify(s, length=13)
-        self.assertEqual('mr%20john%20smith', ''.join(s))
+        self.assertEqual(list('mr%20john%20smith'), s)
 
 
 if __name__ == '__main__':
