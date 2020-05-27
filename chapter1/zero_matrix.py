ed323edb9b6c90d95fce477e430a60354342e74e
Matt Colen <mattcolen@gmail.com>
2020-05-26 21:48:47 -0700

    Document that matrix must not be empty

--- chapter1/zero_matrix.py
+++ chapter1/zero_matrix.py
@@ -11,7 +11,7 @@ def zero_matrix(matrix: Sequence[MutableSequence[int]]) -> None:
     """Sets entire row and column to 0 for all 0 elements in matrix.
 
     Args:
-        matrix: A square 2-D array.
+        matrix: A non-empty square 2-D array.
     """
     m, n = len(matrix), len(matrix[0])
     first_row_zero = 0 in matrix[0]
