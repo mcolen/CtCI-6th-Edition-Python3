17717966c5675eb14849367938350957104293f1
Matt Colen <mattcolen@gmail.com>
2020-06-02 17:16:08 -0700

    Solve 8.2 Robot in a Grid

---
+++ chapter8/robot_in_a_grid.py
@@ -0,0 +1,56 @@
+"""Solution to 8.2 Robot in a Grid.
+
+Imagine a robot sitting on the upper left corner of grid with `r` rows
+and `c` columns. The robot can only move in two directions, right and
+down, but certain cells are "off limits" such that the robot cannot step
+on them. Design an algorithm to find a path for the robot from the top
+left to the bottom right.
+"""
+
+import enum
+from typing import List, NamedTuple, Optional, Set, Sequence
+
+
+class Direction(enum.Enum):
+    """A cardinal direction that the robot can travel."""
+    DOWN = 1
+    RIGHT = 2
+
+
+class _Point(NamedTuple):
+    """A point in a grid."""
+    row: int
+    column: int
+
+
+def find_path(grid: Sequence[Sequence[bool]]) -> Optional[List[Direction]]:
+    """Finds a path for robot from top left to bottom right of grid.
+
+    Args:
+        grid: A non-empty 2-d array of bools. A value of False indicates
+            cells in the grid that are "off limits" to the robot.
+
+    Returns:
+        Sequence of DOWN/RIGHT that will take the robot from top left to
+            bottom right without traversing "off limits" cells, or None
+            if no such sequence exists.
+    """
+    path: List[Direction] = []
+    visited: Set[_Point] = set()
+
+    def helper(r: int, c: int) -> bool:
+        # Tries to get to (0, 0) from (r, c).
+        if not grid[r][c] or _Point(r, c) in visited:
+            return False
+        if r == 0 and c == 0:
+            return True
+        if r > 0 and helper(r - 1, c):
+            path.append(Direction.DOWN)
+            return True
+        if helper(r, c - 1):
+            path.append(Direction.RIGHT)
+            return True
+        visited.add(_Point(r, c))
+        return False
+
+    return path if helper(len(grid) - 1, len(grid[-1]) - 1) else None
---
+++ chapter8/tests/test_robot_in_a_grid.py
@@ -0,0 +1,34 @@
+"""Tests for chapter8.robot_in_a_grid."""
+
+import unittest
+
+from chapter8 import robot_in_a_grid
+
+DOWN = robot_in_a_grid.Direction.DOWN
+RIGHT = robot_in_a_grid.Direction.RIGHT
+
+
+class TestRobotInAGrid(unittest.TestCase):
+
+    def test_exists_path(self) -> None:
+        grid = [
+            [True, True, True, True, False],
+            [True, False, False, False, True],
+            [True, True, True, True, True],
+            [True, True, True, False, True],
+        ]
+        self.assertEqual([DOWN, DOWN, RIGHT, RIGHT, RIGHT, RIGHT, DOWN],
+                         robot_in_a_grid.find_path(grid))
+
+    def test_no_path(self) -> None:
+        grid = [
+            [True, True, True, True, False],
+            [True, False, False, False, True],
+            [True, True, False, True, True],
+            [True, True, True, False, True],
+        ]
+        self.assertIsNone(robot_in_a_grid.find_path(grid))
+
+
+if __name__ == '__main__':
+    unittest.main()
