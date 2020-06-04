44c2f1d0e820ae49528c3a2aef8f67b15b8f50b4
Matt Colen <mattcolen@gmail.com>
2020-06-04 12:42:46 -0700

    Solve Power Set without itertools

--- chapter8/power_set.py
+++ chapter8/power_set.py
@@ -3,7 +3,6 @@
 Write a method to return all subsets of a set.
 """
 
-import itertools
 from typing import AbstractSet, FrozenSet, Set, TypeVar
 
 T = TypeVar('T')
@@ -11,6 +10,12 @@ T = TypeVar('T')
 
 def power_set(set_: AbstractSet[T]) -> Set[FrozenSet[T]]:
     """Returns the set of all subsets of given set_."""
-    subsets = itertools.chain.from_iterable(itertools.combinations(set_, size)
-                                            for size in range(len(set_) + 1))
-    return {frozenset(subset) for subset in subsets}
+    list_ = list(set_)  # Get a stable iteration order.
+    subsets = set()
+    for i in range(1 << len(set_)):
+        subset = set()
+        for j, item in enumerate(list_):
+            if (1 << j) & i:
+                subset.add(item)
+        subsets.add(frozenset(subset))
+    return subsets
