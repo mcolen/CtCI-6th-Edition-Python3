a667473f5c938a73c352fa933259f913de0adcf1
Matt Colen <mattcolen@gmail.com>
2020-06-04 17:55:06 -0700

    Solve 8.9 Parens

---
+++ chapter8/parens.py
@@ -0,0 +1,36 @@
+"""Solution to 8.9 Parens.
+
+Implement an algorithm to print all valid (e.g., properly opened and
+closed) combinations of n pairs of parentheses.
+
+EXAMPLE
+Input:  3
+Output: ((())), (()()), (())(), ()(()), ()()()
+"""
+
+from typing import NamedTuple
+
+
+class _Node(NamedTuple):
+    ending: str
+    opened: int
+    closed: int
+
+
+def print_valid_combinations(n: int) -> None:
+    """Prints all valid combinations of n pairs of parentheses."""
+    nodes = [_Node(ending='', opened=0, closed=0)]
+    for _ in range(2 * n):
+        next_nodes = []
+        for ending, opened, closed in nodes:
+            if closed < n:
+                next_nodes.append(_Node(ending=')' + ending,
+                                        opened=opened, closed=closed + 1))
+            if opened < closed:
+                next_nodes.append(_Node(ending='(' + ending,
+                                        opened=opened + 1, closed=closed))
+        nodes = next_nodes
+    print(', '.join(node.ending for node in nodes))
+
+
+print_valid_combinations(3)
---
+++ chapter8/tests/test_parens.py
@@ -0,0 +1,21 @@
+"""Tests for chapter8.parens."""
+
+import io
+import unittest
+import unittest.mock
+
+from chapter8 import parens
+
+
+class TestParens(unittest.TestCase):
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_3(self, mock_stdout: io.StringIO) -> None:
+        parens.print_valid_combinations(n=3)
+        self.assertEqual(
+            {'((()))', '(()())', '(())()', '()(())', '()()()'},
+            set(mock_stdout.getvalue()[:-1].split(', ')))
+
+
+if __name__ == '__main__':
+    unittest.main()
