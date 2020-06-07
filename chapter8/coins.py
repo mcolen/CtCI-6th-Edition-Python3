16580933ee083e484d032412dc4d1101944a59cb
Matt Colen <mattcolen@gmail.com>
2020-06-07 13:43:23 -0700

    Implement coin_combinations bottom-up

--- chapter8/coins.py
+++ chapter8/coins.py
@@ -5,9 +5,6 @@ nickles (5 cents), and pennies (1 cent), write code to calculate the
 number of ways of representing `n` cents.
 """
 
-import functools
-from typing import FrozenSet
-
 
 def coin_combinations(n: int) -> int:
     """Returns the number of ways of representing n cents.
@@ -17,14 +14,8 @@ def coin_combinations(n: int) -> int:
     """
     if n < 0:
         return 0
-    return _coin_combinations_internal(n, denoms=frozenset([1, 5, 10, 25]))
-
-
-@functools.lru_cache(maxsize=None)
-def _coin_combinations_internal(n: int, denoms: FrozenSet[int]) -> int:
-    if n == 0 or len(denoms) == 1:
-        return 1
-    max_denom = max(denoms)
-    remaining_denoms = frozenset(denoms - {max_denom})
-    return sum(_coin_combinations_internal(n - i * max_denom, remaining_denoms)
-               for i in range(n // max_denom + 1))
+    dp = [1] + [0] * n
+    for coin in [1, 5, 10, 25]:
+        for i in range(len(dp) - coin):
+            dp[i + coin] += dp[i]
+    return dp[-1]
