dd8def432451138f486beac7458017ab67831b3b
Matt Colen <mattcolen@gmail.com>
2020-06-07 22:07:37 -0700

    Solve First Common Ancestor without parent link

--- chapter4/first_common_ancestor.py
+++ chapter4/first_common_ancestor.py
@@ -5,33 +5,65 @@ two nodes in a binary tree. Avoid storing additional nodes in a data
 structure. NOTE: This is not necessarily a binary search tree.
 """
 
-from typing import Optional, TypeVar
+import dataclasses
+from typing import Generic, Optional, TypeVar
 
 from chapter4 import tree
 
 T = TypeVar('T')
 
 
-def first_common_ancestor(
-        node1: tree.NodeWithParent[T],
-        node2: tree.NodeWithParent[T]) -> Optional[tree.NodeWithParent[T]]:
-    """Returns first common ancestor of node1 and node2."""
-    depth1, depth2 = 0, 0
-    float1, float2 = node1, node2
-    while float1.parent:
-        depth1, float1 = depth1 + 1, float1.parent
-    while float2.parent:
-        depth2, float2 = depth2 + 1, float2.parent
-    if float1 is not float2:
-        return None
+def first_common_ancestor(root: tree.Node[T],
+                          node1: tree.Node[T],
+                          node2: tree.Node[T]) -> Optional[tree.Node[T]]:
+    """Finds the first common ancestor of two nodes in a binary tree.
 
-    for _ in range(depth1 - depth2):
-        assert node1.parent
-        node1 = node1.parent
-    for _ in range(depth2 - depth1):
-        assert node2.parent
-        node2 = node2.parent
-    while node1 is not node2:
-        assert node1.parent and node2.parent
-        node1, node2 = node1.parent, node2.parent
-    return node1
+    Args:
+        root: The root of the tree in which to look for ancestors.
+        node1: The first node for which to find an ancestor.
+        node2: The second node for which to find an ancestor.
+
+    Returns:
+        The first common ancestor in tree with given root of both
+        argument node1 and argument node2, or None if at least one of
+        node1 and node2 is not in the tree with given root.
+    """
+    return _search(root, node1, node2).first_common_ancestor
+
+
+@dataclasses.dataclass
+class _SearchResult(Generic[T]):
+    has_node1: bool
+    has_node2: bool
+    first_common_ancestor: Optional[tree.Node[T]]
+
+
+def _search(root: tree.Tree[T],
+            node1: tree.Node[T],
+            node2: tree.Node[T]) -> _SearchResult[T]:
+    if not root:
+        return _SearchResult(has_node1=False,
+                             has_node2=False,
+                             first_common_ancestor=None)
+    if root is node1 and root is node2:
+        return _SearchResult(has_node1=True,
+                             has_node2=True,
+                             first_common_ancestor=root)
+    left_result = _search(root.left, node1, node2)
+    if left_result.first_common_ancestor:
+        return left_result
+    has_node1 = left_result.has_node1 or root is node1
+    has_node2 = left_result.has_node2 or root is node2
+    if has_node1 and has_node2:
+        return _SearchResult(has_node1=True,
+                             has_node2=True,
+                             first_common_ancestor=root)
+    right_result = _search(root.right, node1, node2)
+    if right_result.first_common_ancestor:
+        return right_result
+    has_node1 |= right_result.has_node1
+    has_node2 |= right_result.has_node2
+    return _SearchResult(
+        has_node1,
+        has_node2,
+        first_common_ancestor=root if has_node1 and has_node2 else None)
--- chapter4/tests/test_first_common_ancestor.py
+++ chapter4/tests/test_first_common_ancestor.py
@@ -12,23 +12,27 @@ class TestFirstCommonAncestor(unittest.TestCase):
         bst2 = minimal_tree.minimal_bst(range(7))
         assert bst1.left and bst2.right
         self.assertIsNone(
-            first_common_ancestor.first_common_ancestor(bst1.left, bst2.right))
+            first_common_ancestor.first_common_ancestor(root=bst1,
+                                                        node1=bst1.left,
+                                                        node2=bst2.right))
 
     def test_same_depth(self) -> None:
         bst = minimal_tree.minimal_bst(range(7))
         assert bst.left and bst.left.left and bst.left.right
         self.assertIs(
             bst.left,
-            first_common_ancestor.first_common_ancestor(
-                bst.left.left, bst.left.right))
+            first_common_ancestor.first_common_ancestor(root=bst,
+                                                        node1=bst.left.left,
+                                                        node2=bst.left.right))
 
     def test_different_depth(self) -> None:
         bst = minimal_tree.minimal_bst(range(7))
         assert bst.left and bst.right and bst.right.right
         self.assertIs(
             bst,
-            first_common_ancestor.first_common_ancestor(
-                bst.left, bst.right.right))
+            first_common_ancestor.first_common_ancestor(root=bst,
+                                                        node1=bst.left,
+                                                        node2=bst.right.right))
 
 
 if __name__ == '__main__':
