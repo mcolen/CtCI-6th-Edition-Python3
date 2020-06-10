54715e04cf8a7af4bd2e6e9bd4c1fddffb2576a8
Matt Colen <mattcolen@gmail.com>
2020-06-10 16:12:58 -0700

    Solve 10.4 Sorted Search, No Size

---
+++ chapter10/sorted_search_no_size.py
@@ -0,0 +1,52 @@
+"""Solution to 10.4 Sorted Search, No Size.
+
+You are given an array-like data structure `Listy` which lacks a size
+method. It does, however, have an `elementAt(i)` method that returns the
+element at index `i` in `O(1)` time. If `i` is byeond the bounds of the
+data structure, it returns -1. (For this reason, the data structure only
+supports positive integers.) Given a `Listy` which contains sorted,
+positive integers, find the index at which an element `x` occurs. If `x`
+occurs multiple times, you may return any index.
+"""
+
+from typing import Optional, Sequence
+
+
+class Listy:  # pylint: disable=too-few-public-methods
+    """Array-like data strcuture which lacks a size method."""
+
+    def __init__(self, seq: Sequence[int]) -> None:
+        self._seq = seq
+
+    def __getitem__(self, i: int) -> int:
+        return self._seq[i] if i < len(self._seq) else -1
+
+
+def index(listy: Listy, target: int) -> Optional[int]:
+    """Finds index of target in sorted Listy.
+
+    Args:
+        listy: A Listy of sorted, positive integers.
+        target: An integer for which to search in argument listy.
+
+    Returns:
+        An index at which target can be found in listy, or None if no
+        such index exists.
+    """
+    lo, hi = 0, _bound_size(listy) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if listy[mid] > target or listy[mid] == -1:
+            hi = mid - 1
+        elif listy[mid] < target:
+            lo = mid + 1
+        else:
+            return mid
+    return None
+
+
+def _bound_size(listy: Listy) -> int:
+    bound = 1
+    while listy[bound] != -1:
+        bound *= 2
+    return bound
---
+++ chapter10/tests/test_sorted_search_no_size.py
@@ -0,0 +1,87 @@
+"""Tests for chapter10.sorted_search_no_size."""
+
+import unittest
+
+from chapter10 import sorted_search_no_size
+
+
+class TestSortedSearchNoSize(unittest.TestCase):
+
+    def test_find_1_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(0, sorted_search_no_size.index(listy, target=1))
+
+    def test_find_2_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(1, sorted_search_no_size.index(listy, target=2))
+
+    def test_find_4_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(2, sorted_search_no_size.index(listy, target=4))
+
+    def test_find_5_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(3, sorted_search_no_size.index(listy, target=5))
+
+    def test_find_6_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(4, sorted_search_no_size.index(listy, target=6))
+
+    def test_find_7_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(5, sorted_search_no_size.index(listy, target=7))
+
+    def test_find_9_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(6, sorted_search_no_size.index(listy, target=9))
+
+    def test_find_10_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(7, sorted_search_no_size.index(listy, target=10))
+
+    def test_find_11_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(8, sorted_search_no_size.index(listy, target=11))
+
+    def test_find_12_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(9, sorted_search_no_size.index(listy, target=12))
+
+    def test_find_13_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(10, sorted_search_no_size.index(listy, target=13))
+
+    def test_find_14_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(11, sorted_search_no_size.index(listy, target=14))
+
+    def test_find_16_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(12, sorted_search_no_size.index(listy, target=16))
+
+    def test_find_18_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertEqual(13, sorted_search_no_size.index(listy, target=18))
+
+    def test_find_15_in_1_2_4_5_6_7_9_10_11_12_13_14_16_18(self) -> None:
+        listy = sorted_search_no_size.Listy(
+            [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 16, 18])
+        self.assertIsNone(sorted_search_no_size.index(listy, target=15))
+
+
+if __name__ == '__main__':
+    unittest.main()
