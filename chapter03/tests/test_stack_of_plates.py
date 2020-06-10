15b8cb6e233a081b54a8611624b759f36564c631
Matt Colen <mattcolen@gmail.com>
2020-06-10 16:15:23 -0700

    Avoid loop in test

--- chapter03/tests/test_stack_of_plates.py
+++ chapter03/tests/test_stack_of_plates.py
@@ -10,12 +10,22 @@ IntStack = stack_of_plates.Stack[int]
 
 class TestStackOfPlates(unittest.TestCase):
 
-    def test_34_items_5_capacity(self) -> None:
-        stack = IntStack(capacity=5)
-        for i in range(34):
-            stack.push(i)
-        for i in reversed(range(34)):
-            self.assertEqual(i, stack.pop())
+    def test_7_items_3_capacity(self) -> None:
+        stack = IntStack(capacity=3)
+        stack.push(0)
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        stack.push(4)
+        stack.push(5)
+        stack.push(6)
+        self.assertEqual(6, stack.pop())
+        self.assertEqual(5, stack.pop())
+        self.assertEqual(4, stack.pop())
+        self.assertEqual(3, stack.pop())
+        self.assertEqual(2, stack.pop())
+        self.assertEqual(1, stack.pop())
+        self.assertEqual(0, stack.pop())
         with self.assertRaises(chapter03.stack.EmptyStackError):
             stack.pop()
 
