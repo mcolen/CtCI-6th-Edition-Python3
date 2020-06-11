96b4e833debbb8b2d36f94a2ff4e0fa644835792
Matt Colen <mattcolen@gmail.com>
2020-06-10 18:36:13 -0700

    Solve 10.5 Sparse Search

---
+++ chapter10/sparse_search.py
@@ -0,0 +1,52 @@
+"""Solution to 10.5 Sparse Search.
+
+Given a sorted array of strings that is interspresed with empty strings,
+write a method to find the location of a given string.
+
+EXAMPLE
+Input:
+`ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}`
+Output: 4
+"""
+
+from typing import Optional, Sequence
+
+
+def index(sorted_strings: Sequence[str], target: str) -> Optional[int]:
+    """Finds target in sorted_strings.
+
+    Args:
+        sorted_strings: Sequence of strings in which all the non-empty
+            strings are sorted in ascending order.
+        target: String to find in sorted_strings. May not be empty.
+
+    Returns:
+        An index at which target can be found in sorted_strings, or None
+        if no such index exists.
+    """
+    lo, hi = 0, len(sorted_strings) - 1
+    while lo <= hi:
+        left = right = (lo + hi) // 2
+        # Find middlemost non-empty string.
+        while not sorted_strings[left] and not sorted_strings[right]:
+            if left == lo and right == hi:
+                return None
+            if left > lo:
+                left -= 1
+            if right < hi:
+                right += 1
+        if sorted_strings[right]:
+            if sorted_strings[right] < target:
+                lo = right + 1
+            elif sorted_strings[right] > target:
+                hi = left if left < right else right - 1
+            else:
+                return right
+        else:
+            if sorted_strings[left] < target:
+                lo = right + 1
+            elif sorted_strings[left] > target:
+                hi = left - 1
+            else:
+                return left
+    return None
---
+++ chapter10/tests/test_sparse_search.py
@@ -0,0 +1,61 @@
+"""Tests for chapter10.sparse_search."""
+
+import unittest
+
+from chapter10 import sparse_search
+
+
+class TestSparseSearch(unittest.TestCase):
+
+    def test_find_apple_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(0,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='apple'))
+
+    def test_find_banana_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(3,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='banana'))
+
+    def test_find_carrot_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(7,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='carrot'))
+
+    def test_find_duck_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(8,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='duck'))
+
+    def test_find_eel_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(11,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='eel'))
+
+    def test_find_flower_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertEqual(13,
+                         sparse_search.index(
+                             ['apple', '', '', 'banana', '', '', '', 'carrot',
+                              'duck', '', '', 'eel', '', 'flower'],
+                             target='flower'))
+
+    def test_find_ac_in_apple_banana_carrot_duck_eel_flower(self) -> None:
+        self.assertIsNone(
+            sparse_search.index(
+                ['apple', '', '', 'banana', '', '', '', 'carrot',
+                 'duck', '', '', 'eel', '', 'flower'],
+                target='ac'))
+
+
+if __name__ == '__main__':
+    unittest.main()
