d4e625cee4523a4e097c37a0922b4df525c31c76
Matt Colen <mattcolen@gmail.com>
2020-06-16 13:33:59 -0700

    Use assertCountEqual()

--- chapter04/tests/test_bst_sequences.py
+++ chapter04/tests/test_bst_sequences.py
@@ -10,9 +10,7 @@ class TestBSTSequences(unittest.TestCase):
     def test_2_1_3(self) -> None:
         root = tree.Node(value=2, left=tree.Node(1), right=tree.Node(3))
         sequences = bst_sequences.bst_sequences(root)
-        self.assertEqual({(2, 1, 3), (2, 3, 1)},
-                         {tuple(sequence)
-                          for sequence in sequences})
+        self.assertCountEqual([[2, 1, 3], [2, 3, 1]], sequences)
 
 
 if __name__ == '__main__':
