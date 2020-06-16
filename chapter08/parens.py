c8577e22c76a36030b20fd9d1131e8f4087726ba
Matt Colen <mattcolen@gmail.com>
2020-06-16 14:04:42 -0700

    Return str instead of printing where convenient

--- chapter08/parens.py
+++ chapter08/parens.py
@@ -9,28 +9,29 @@ Output: ((())), (()()), (())(), ()(()), ()()()
 """
 
 import dataclasses
+from typing import Set
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class _Node:
     ending: str
     opened: int
     closed: int
 
 
-def print_valid_combinations(n: int) -> None:
-    """Prints all valid combinations of n pairs of parentheses."""
-    nodes = [_Node(ending='', opened=0, closed=0)]
+def valid_combinations(n: int) -> Set[str]:
+    """Returns all valid combinations of n pairs of parentheses."""
+    nodes = {_Node(ending='', opened=0, closed=0)}
     for _ in range(2 * n):
-        next_nodes = []
+        next_nodes = set()
         for node in nodes:
             if node.closed < n:
-                next_nodes.append(_Node(ending=')' + node.ending,
-                                        opened=node.opened,
-                                        closed=node.closed + 1))
+                next_nodes.add(_Node(ending=')' + node.ending,
+                                     opened=node.opened,
+                                     closed=node.closed + 1))
             if node.opened < node.closed:
-                next_nodes.append(_Node(ending='(' + node.ending,
-                                        opened=node.opened + 1,
-                                        closed=node.closed))
+                next_nodes.add(_Node(ending='(' + node.ending,
+                                     opened=node.opened + 1,
+                                     closed=node.closed))
         nodes = next_nodes
-    print(', '.join(node.ending for node in nodes))
+    return set(node.ending for node in nodes)
--- chapter08/tests/test_parens.py
+++ chapter08/tests/test_parens.py
@@ -1,20 +1,15 @@
 """Tests for chapter08.parens."""
 
-import io
 import unittest
-import unittest.mock
 
 from chapter08 import parens
 
 
 class TestPrintValidCombinations(unittest.TestCase):
 
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_3(self, mock_stdout: io.StringIO) -> None:
-        parens.print_valid_combinations(n=3)
-        self.assertCountEqual(
-            ['((()))', '(()())', '(())()', '()(())', '()()()'],
-            mock_stdout.getvalue()[:-1].split(', '))
+    def test_3(self) -> None:
+        self.assertEqual({'((()))', '(()())', '(())()', '()(())', '()()()'},
+                         parens.valid_combinations(n=3))
 
 
 if __name__ == '__main__':
