20b2225f22f7bb42f2c49f2af563b629fed30050
Matt Colen <mattcolen@gmail.com>
2020-06-18 00:07:31 -0700

    Remove unnecessary assertion

--- chapter10/tests/test_group_anagrams.py
+++ chapter10/tests/test_group_anagrams.py
@@ -10,7 +10,6 @@ class TestSort(unittest.TestCase):
     def test_apple_ele_papel_eel_lee_elppa(self) -> None:
         strings = ['apple', 'ele', 'papel', 'eel', 'lee', 'elppa']
         group_anagrams.sort(strings)
-        self.assertEqual(6, len(strings))
         self.assertCountEqual(
             [{'apple', 'papel', 'elppa'}, {'ele', 'eel', 'lee'}],
             [set(strings[:3]), set(strings[3:])])
